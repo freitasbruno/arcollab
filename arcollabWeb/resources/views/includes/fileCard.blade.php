5abafdd7d2e64ec4331f4334fcc7f5105149e0b9
Bruno Freitas <freitascbruno@gmail.com>
2017-06-02 20:51:41 +0530

    Displaying images thumbnail

--- arcollabWeb/resources/views/includes/fileCard.blade.php
+++ arcollabWeb/resources/views/includes/fileCard.blade.php
@@ -1,10 +1,16 @@
 <div class="uk-margin-top">
 	<div class="uk-card uk-card-default uk-margin-remove uk-margin-top" uk-grid>
 	    <div class="uk-card-media-left uk-cover-container uk-width-1-4 uk-padding-remove">
-			<a class="uk-link-muted" href="/uploads/{!! $file->filename !!}" target="_blank">
+			<a class="uk-link-muted" href="#modal-center" uk-toggle>
 		        <img src="/uploads/{!! $file->filename !!}" alt="{!! $file->filename !!}" uk-cover>
 		        <canvas width="300" height="150"></canvas>
 			</a>
+			<div id="modal-center" uk-modal="center: true">
+			    <div class="uk-modal-dialog">
+			        <button class="uk-modal-close-outside" type="button" uk-close></button>
+			        <img src="/uploads/{!! $file->filename !!}" alt="{!! $file->filename !!}">
+			    </div>
+			</div>
 	    </div>
 		<div class="uk-card-body">
             <h4>{!! $file->originalName !!}</h4>
